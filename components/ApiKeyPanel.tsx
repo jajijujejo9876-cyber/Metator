8323e88199773e0b34d986632ab4cc7ec7a2479d
jajijujejo9876-cyber <jajijujejo9876@gmail.com>
2026-02-23 00:57:13 +0700

    Remove unnecessary blank line in ApiKeyPanel.tsx

--- components/ApiKeyPanel.tsx
+++ components/ApiKeyPanel.tsx
@@ -3,7 +3,6 @@ import { Key, Plus, Trash2, XCircle, ListOrdered, Search, LogIn, ShieldCheck, Sa
 import { AppMode, ApiProvider } from '../types';
 import { PUTER_MODELS } from '../constants';
 
-
 interface Props {
   apiKeys: string[];
   setApiKeys: (keys: string[]) => void;
